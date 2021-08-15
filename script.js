d3fb98481f7824493cb5ee74ce563c2dda161409
shuman-hasan <88977932+shuman-hasan@users.noreply.github.com>
2021-08-15 21:28:08 +0600

    Add files via upload
    html,css,bootstrap,javaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.banner-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:1400,
+      
+        
+      });
+})
